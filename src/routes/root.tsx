650dd51e964da767c6081d874003c64a308acfb6
Joan <joan300501@gmail.com>
2024-01-05 18:44:49 -0500

    Refactor root.tsx to use grid layout and add ArticleCard

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,5 @@
 import AppLayout from "@/components/layout/app.layout";
+import { ArticleCard } from "@/components/root/article.card";
 import CardDashboard from "@/components/root/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -63,35 +64,36 @@ export default function Root() {
             Last Updated <span className="font-bold">12:40 PM</span>
           </p>
         </div>
-        <div className="bg-white ">
-          <div className="flex justify-between gap-5">
-            <CardDashboard
-              color="bg-purple-100"
-              title="New Subscribers"
-              count={3012}
-              characterizer="people"
-              percentage="10"
-            >
-              <LucidePersonStanding size={20} />
-            </CardDashboard>
-            <CardDashboard
-              color="bg-green-100"
-              title="Total Views"
-              count={3012}
-              characterizer="views"
-              percentage="10"
-            >
-              <LucideEye className="fill-black stroke-white" size={20} />
-            </CardDashboard>
-            <CardDashboard
-              color="bg-indigo-100"
-              title="Engagement Rate"
-              count={3012}
-              characterizer="%"
-              percentage="10"
-            >
-              <LucideHeart className="fill-black" size={20} />
-            </CardDashboard>
+        <div className="grid grid-flow-row grid-cols-6">
+          <CardDashboard
+            color="bg-purple-100"
+            title="New Subscribers"
+            count={3012}
+            characterizer="people"
+            percentage="10"
+          >
+            <LucidePersonStanding size={20} />
+          </CardDashboard>
+          <CardDashboard
+            color="bg-green-100"
+            title="Total Views"
+            count={3012}
+            characterizer="views"
+            percentage="10"
+          >
+            <LucideEye className="fill-black stroke-white" size={20} />
+          </CardDashboard>
+          <CardDashboard
+            color="bg-indigo-100"
+            title="Engagement Rate"
+            count={3012}
+            characterizer="%"
+            percentage="10"
+          >
+            <LucideHeart className="fill-black" size={20} />
+          </CardDashboard>
+          <div className="col-span-3 row-span-2">
+            <ArticleCard />
           </div>
         </div>
       </div>
