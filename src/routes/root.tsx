bc9da18c0cdb11127d8f9626a43830b86dc42c7c
Brayan Paucar <joan300501@gmail.com>
2024-02-15 12:46:54 -0500

    Landing page with UX, and forgot password frame improved

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link, Outlet } from "react-router-dom";
 import { SignIn } from "@clerk/clerk-react";
+import { MoveRight } from "lucide-react";
 import Gradient from "@/assets/gradient.png";
 export default function Root() {
   return (
@@ -14,13 +15,27 @@ export default function Root() {
       className="flex justify-center  h-screen items-center"
     >
       <SignIn path="/login" routing="path" />
-      <div className="flex flex-col gap-5 items-center">
+      <div className="flex flex-col gap-8 items-center w-1/2">
         <img
-          className="mx-auto cursor-pointer active:opacity-80"
+          width={80}
+          height={80}
+          className="rounded-full shadow-lg"
           src="https://cdn-icons-png.flaticon.com/128/7721/7721025.png"
         />
+        <h1 className="scroll-m-20 text-4xl bg-clip-text text-transparent bg-gradient-to-tr from-black to-zinc-400 via-zinc-500 font-extrabold tracking-tight lg:text-5xl">
+          Kite Space
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-3 text-center">
+          A platform for content creators to track their analytics and monetize
+          their content. We provide a platform for creators to connect—
+          <span className="underline">with their audience</span> and —
+          <span className="underline">grow their brand.</span>
+        </p>
         <Link to="/login">
-          <Button>Login to Continue</Button>
+          <Button className="group px-8 rounded-lg">
+            Login to Continue
+            <MoveRight className=" ml-2 duration-500 group-hover:translate-x-3" />
+          </Button>
         </Link>
       </div>
       <Outlet />
